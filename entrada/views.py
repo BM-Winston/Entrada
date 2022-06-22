b05b424441f7086da4d6054d0c6f1a74bfa2a797
BM-Winston <winston.bwosi@student.moringaschool.com>
2022-06-22 23:49:32 +0300

    add views importations

--- entrada/views.py
+++ entrada/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from entrada.forms import RegisterForm
+import email
+from django.contrib import messages
+from .models import *
 
-# Create your views here.
+
